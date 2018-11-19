85295c24ddc8f2fbd160c725e565a1129deda254
kyle <kyledr215@gmail.com>
2018-11-19 16:24:08 -0500

    boat creating created

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,17 +1,44 @@
 class BoatsController < ApplicationController
   def index
+    @boat = Boat.all
   end
 
   def new
+    @boats = Boat.new
+  end
+
+  def create
+    boat = Boat.new(boats_params)
+    boat.save
+    if boat.save
+      redirect_to boats_path
+    else
+      redirect_to new_boat_path
+    end
   end
 
   def edit
+    @boat = Boat.find(params[:id])
+  end
+
+  def update
+    boat = Boat.find(params[:id])
+    if boat.update(boats_params)
+      redirect_to boats_path
+    else
+      render 'boats/#{boats.id}/edit'
+    end
   end
 
   def delete
+    boat = Boat.find(params[:id])
+    if boat.destroy
+      redirect_to root_path
+    end
   end
 
   def show
+    @boat = Boat.find(params[:id])
   end
 
   private
