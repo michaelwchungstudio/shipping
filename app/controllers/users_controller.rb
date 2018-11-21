ac5bdf66d85939a627c2c84e037c52a2c9dd2923
Manny <devolthdd@gmail.com>
2018-11-21 12:51:15 -0500

    last form incomplete right now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,23 @@ class UsersController < ApplicationController
     @jobs = Job.where(user_id: params[:id])
   end
 
+  def admin
+
+  	@user = User.find(params[:user_id])
+  	p "dasjashdsahjskdhajsdhadhljs"
+  	p params
+  	 if @user.update(admin?: params[:admin?])
+  		redirect_to "/"
+  	 else
+  	 	render "/users/profile/#{@user.id}"
+  	 end
+
+  	
+  end
+  private
+  def user_params
+  	params.require(:user).permit(:admin?)
+  end
+  def user_params2
+  end
 end
